f7669769e5a8f2fa08e27b4f6f6c7def690b166d
Andrew <andhochmuth@gmail.com>
2022-12-30 20:12:38 -0500

    Initial build for the API.

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const getProductData = require('get-product-name');
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+function sendError(res, err){
+    res.statusCode = err
+    res.end();
+}
+
+async function getProduct(res, productUrl){
+    try{
+        const productData = await getProductData(productUrl);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(productData));
+    }catch (err) {
+        sendError(res, 500)
+    }
+}
+
+app.get('/', (req,res) => {
+    sendError(res, 405)
+})
+
+app.post('/', (req, res) => {  
+    const productUrl = req.body.productUrl
+
+    //Check if ProductURL was sent
+    if(productUrl === undefined || productUrl === ""){
+        sendError(res, 418)
+        return
+    }
+    
+    getProduct(res, productUrl)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
